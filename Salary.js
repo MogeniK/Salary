6d36c20eaff7c1fdf2bd493039e77295de7d9bd1
Sergiy Korolchuk <70841259+MogeniK@users.noreply.github.com>
2024-11-20 10:20:00 +0200

    Add files via upload

---
+++ Salary.js
@@ -0,0 +1,22 @@
+const productName = "iPhone 14 Pro";
+const releaseYear = 2023; 
+let productPrice = 45000;
+let inStock = false;
+let customerName = "Андрій";
+console.log(`Товар: ${productName}, рік випуску ${releaseYear}, ціна ${productPrice} гривень, В наявності: ${inStock}, Покупець: ${customerName}`);
+
+
+
+
+
+
+
+
+let yearsOFFemploment = 6;
+let salary;
+if (yearsOFFemploment === 1 ) {
+    salary = 1000-100;
+ } else {
+    salary = 1000 * yearsOFFemploment
+ }
+console.log(salary)
